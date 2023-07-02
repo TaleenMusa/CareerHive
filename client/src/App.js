691cd14656e7558a182411d1f49ae703092a932c
TaleenMusa <taleenkhoury20@gmail.com>
2023-07-03 01:46:42 +0300

    Add the Job list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import { Form, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-
+import Home from './views/Home/Home';
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/" element={<Form />} />
+      {/* <Route path="/" element={<Form />} /> */}
       </Routes>
     </div>
   );
--- client/src/components/JobList/JobList.js
+++ client/src/components/JobList/JobList.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import jobData from './jobData'; 
+// import React from 'react';
+// import jobData from './jobData'; 
 
-const JobList = () => {
-  return (
-    <div>
-      {jobData.map((job) => (
-        <div key={job.id}>
-          <h3>{job.title}</h3>
-          <p>Category: {job.category}</p>
-          <p>Location: {job.location}</p>
-          <p>Publish Date: {job.publishDate}</p>
-          <p>Deadline: {job.deadline}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+// const JobList = () => {
+//   return (
+//     <div>
+//       {jobData.map((job) => (
+//         <div key={job.id}>
+//           <h3>{job.title}</h3>
+//           <p>Category: {job.category}</p>
+//           <p>Location: {job.location}</p>
+//           <p>Publish Date: {job.publishDate}</p>
+//           <p>Deadline: {job.deadline}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default JobList;
+// export default JobList;
