e24138172bf3a36ce0988622a95598c082e1198b
mahdi saqqa <mahdi.saqqa@gmail.com>
2023-07-03 16:14:50 +0300

    setting user model, controller, routs, and jwt

---
+++ server/models/user.model.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+
+
+const UserSchema = new mongoose.Schema({
+    Fname: {
+      type: String,
+      required: [true, "Username is required"] 
+    },
+    Lname: {
+        type: String,
+        required: [true, "Username is required"] 
+      },
+      Company: {
+        type: String,
+        required: [true, "Username is required"] 
+      },
+      Email: {
+      type: String,
+      required: [true, "Email is required"],
+      validate: {
+        validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+        message: "Please enter a valid email"
+      }
+    },
+    Password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be 8 characters or longer"]
+    },
+    Bday: {
+        type: Date,
+        required: [true, "Birthday is required"],
+        validate: {
+            validator: val => val-Date.now() < 18,
+            message: "Age must be over 18"
+        }
+    },
+
+  }, {timestamps: true});
+
+
+UserSchema.virtual("Cpassword")
+  .get(function(){
+      return this._Cpassword;
+  })
+  .set(function(value){
+      this._Cpassword = value
+  })
+
+UserSchema.pre("validate", function(next){
+    if(this.Password !== this.Cpassword){
+        this.invalidate("Cpassword", "Passwords must match")
+    }
+    //save the user to db or show validation errors
+    next();
+})
+
+
+UserSchema.pre("save", function(next){
+    bcrypt.hash(this.Password, 10)
+        .then(hash=>{
+            this.Password = hash
+            next()
+        })
+        .catch(err=>{
+            console.log("HASHING PASSWORD DIDNT WORK THO", err)
+            next()
+        })
+})
+
+
+module.exports = mongoose.model("User", UserSchema);
+
+
--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
-const exress = require('express');
-const app = exress();
-const cors = require('cors');
-require('./config/mongoose.config');
-app.use(cors());
-app.use(exress.json());
-app.use(exress.urlencoded({extended:true}));
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
-})
+const express = require("express");
+const cors = require("cors")
+const cookies = require("cookie-parser");
+
+const port = 8000;
+
+const app = express();
+
+app.use(cors({
+    credentials: true, origin: 'http://localhost:3000'
+}));
+
+app.use(express.json())
+app.use(cookies());
+
+
+//require our mongoose config file and tell it about the db name
+require("./config/mongoose.config")
+
+//require our routes and tell it about our app
+require("./routes/user.routes")(app)
+
+
+app.listen(
+    port,
+    ()=> console.log("listening on port", port)
+)
