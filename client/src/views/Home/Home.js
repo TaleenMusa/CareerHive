25a737898fc216d94ce065680be27fd7ee27b59d
TaleenMusa <taleenkhoury20@gmail.com>
2023-07-03 13:07:11 +0300

    Adj Home page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 // import JobList from '../../components/JobList/JobList'
+import SearchBox from '../components/SearchBox/SearchBox';
 const Home = () => {
   return (
     <div>
+          <SearchBox />
+
     <h1>All Jobs</h1>
     {/* <JobList /> */}
   </div>
