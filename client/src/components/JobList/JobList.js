ce6b5c35cb35d54c53d1f4e4641112a7e3b7094c
TaleenMusa <taleenkhoury20@gmail.com>
2023-07-04 11:02:59 +0300

    Some CSS for the home page

--- client/src/components/JobList/JobList.js
+++ client/src/components/JobList/JobList.js
@@ -1,20 +1,86 @@
-// import React from 'react';
-// import jobData from './jobData'; 
+    import React from 'react';
 
-// const JobList = () => {
-//   return (
-//     <div>
-//       {jobData.map((job) => (
-//         <div key={job.id}>
-//           <h3>{job.title}</h3>
-//           <p>Category: {job.category}</p>
-//           <p>Location: {job.location}</p>
-//           <p>Publish Date: {job.publishDate}</p>
-//           <p>Deadline: {job.deadline}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+    const JobList = () => {
+    const jobData = [
+        {
+        id: 1,
+        title: 'Job Title 1',
+        category: 'Category 1',
+        location: 'Location 1',
+        publishDate: '2023-07-01',
+        deadline: '2023-07-15',
+        },
+        {
+        id: 2,
+        title: 'Job Title 2',
+        category: 'Category 2',
+        location: 'Location 2',
+        publishDate: '2023-07-02',
+        deadline: '2023-07-16',
+        },
+        // Add more job objects as needed
+    ];
 
-// export default JobList;
+    const tableStyle = {
+        borderCollapse: 'collapse',
+        width: '100%',
+    };
+
+    const thStyle = {
+        border: '1px solid #ccc',
+        padding: '8px',
+        backgroundColor: '#f2f2f2',
+        textAlign: 'left',
+    };
+
+    const tdStyle = {
+        border: '1px solid #ccc',
+        padding: '8px',
+    };
+
+    const evenRowStyle = {
+        backgroundColor: '#f9f9f9',
+    };
+
+    const hoverRowStyle = {
+        backgroundColor: '#e9e9e9',
+    };
+
+    return (
+        <div style={{ marginTop: '1rem' }}>
+        <table style={tableStyle}>
+            <thead>
+            <tr>
+                <th style={thStyle}>Title</th>
+                <th style={thStyle}>Category</th>
+                <th style={thStyle}>Location</th>
+                <th style={thStyle}>Publish Date</th>
+                <th style={thStyle}>Deadline</th>
+            </tr>
+            </thead>
+            <tbody>
+            {jobData.map((job, index) => (
+                <tr
+                key={job.id}
+                style={index % 2 === 0 ? evenRowStyle : {}}
+                onMouseEnter={(e) => {
+                    e.target.style.backgroundColor = hoverRowStyle.backgroundColor;
+                }}
+                onMouseLeave={(e) => {
+                    e.target.style.backgroundColor = evenRowStyle.backgroundColor;
+                }}
+                >
+                <td style={tdStyle}>{job.title}</td>
+                <td style={tdStyle}>{job.category}</td>
+                <td style={tdStyle}>{job.location}</td>
+                <td style={tdStyle}>{job.publishDate}</td>
+                <td style={tdStyle}>{job.deadline}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </div>
+    );
+    };
+
+    export default JobList;
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import JobList from '../../components/JobList/JobList'
-import SearchBox from '../components/SearchBox/SearchBox';
+import JobList from '../../components/JobList/JobList'
+import SearchBox from '../../components/Search/SearchBox';
 const Home = () => {
   return (
     <div>
           <SearchBox />
 
     <h1>All Jobs</h1>
-    {/* <JobList /> */}
+    <JobList />
   </div>
   )
 }
