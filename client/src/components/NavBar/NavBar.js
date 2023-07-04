a4f143413514fe0be47748be8bf705395db786b7
TaleenMusa <taleenkhoury20@gmail.com>
2023-07-04 19:28:30 +0300

    Adjust the search box

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const NavBar = () => {
   const menuItems = [
     { label: 'Home', path: '/' },
-    { label: 'Add job', path: '/Add' },
+    { label: 'Add job', path: '/add' },
     { label: 'My job', path: '/my-job' },
   ];
 
